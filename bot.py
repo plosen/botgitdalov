ac08479f266a0bd30b9031be9f1ae3e879593c20
Kirill Danilov <kirilldan1123@gmail.com>
2024-08-29 21:19:54 +0200

    changefromdalov2

--- bot.py
+++ bot.py
@@ -49,4 +49,9 @@ def echo_message(message):
         # Ответ на обычное сообщение
         bot.reply_to(message, message.text)
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
 bot.infinity_polling(none_stop=True)
